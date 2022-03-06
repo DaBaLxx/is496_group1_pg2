a485a608ae7bddf976ef86757b5f82ea39df41bf
DaBaLxx <93062082+DaBaLxx@users.noreply.github.com>
2022-03-06 01:33:55 -0600

    Add files via upload

--- client/tcpclient.py
+++ client/tcpclient.py
@@ -11,7 +11,7 @@
 
 
 # Import any necessary libraries below
-import socket, sys, struct
+import socket, sys, struct, time, subprocess
 
 
 ############## Beginning of Part 1 ##############
@@ -42,7 +42,7 @@ def init_sock(host, port):
 def part1 ():
     # fill in the hostname and port number
     hostname = 'student00.ischool.illinois.edu'
-    port = 41022
+    port = 41029
 
     # A dummy message (in bytes) to test the code
     message = b'Hello World'
@@ -75,6 +75,151 @@ def part1 ():
 # List all the files and directories in server directory
 def ls(sock):
     print(sock.recv(BUFFER).decode('utf-8'))
+
+
+def dn(sock, command):
+
+    # get file size from server and file name from command
+    file_size = struct.unpack('i', sock.recv(BUFFER))[0]
+    filename = command[3:]
+
+    # file exists on server side
+    if file_size != -1:
+
+        f = open(filename, 'wb')
+        recv_size = 0
+
+        # start time
+        start = time.time()
+
+        # receiving file
+        while recv_size < file_size:
+            data = sock.recv(BUFFER)
+            f.write(data)
+            recv_size += len(data)
+
+        # end time
+        end = time.time()
+
+        # speed
+        speed = round(recv_size / 1000000 / (end - start), 2)
+
+        # print throughput
+        print('{} bytes have been transfered in {} seconds: {} Megabytes/second'.format(recv_size, round(end-start, 6), speed))
+    
+    # file does not exist on server side
+    else:
+        print('File does not exist!')
+
+
+def up(sock, command):
+    filename = command[3:]
+
+    # check if the file exists and send ackowledgement to client
+    result = subprocess.run(['ls', '-l', filename], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    
+    # file exists
+    if result.stdout:
+
+        # send a confirmation to server
+        sock.send(struct.pack('i', 1))
+
+        # send file name to server
+        sock.send(filename.encode('utf-8'))
+
+        # receive ackowledgement from server
+        acknowledgement = struct.unpack('i', sock.recv(BUFFER))[0]
+
+        # get the size of this file
+        file_size = int(result.stdout.decode('utf-8').split(' ')[4])
+
+        # file does not exist on server side
+        if acknowledgement == -1:
+
+            # send file size to server
+            sock.send(struct.pack('i', file_size))
+
+            # send file
+            with open(filename, 'rb') as f:
+                for line in f:
+                    sock.send(line)
+            
+            # print throughput
+            throughput = sock.recv(BUFFER).decode('utf-8')
+            print(throughput)
+        
+        # file exists on server side
+        else:
+
+            # ask user if to overwrite the file
+            confirm = input('File already exists. Do you want to overwrite the original file? ')
+            confirm = confirm.lower()
+
+            if confirm == 'no':
+
+                # send a negative confirmation to server
+                sock.send(struct.pack('i', -1))
+                print('Upload abandoned by user.')
+
+            elif confirm == 'yes':
+
+                # send file size to server
+                sock.send(struct.pack('i', file_size))
+                
+                # send file
+                with open(filename, 'rb') as f:
+                    for line in f:
+                        sock.send(line)
+                
+                # print throughput
+                throughput = sock.recv(BUFFER).decode('utf-8')
+                print(throughput)
+            
+            else:
+                # send a negative confirmation to server
+                sock.send(struct.pack('i', -1))
+                print('Failed to resolve command.')
+    
+    # file does not exists
+    if result.stderr:
+
+        # send a negative confirmation to server
+        sock.send(struct.pack('i', -1))
+        print('File does not exist!')
+
+
+def rm(sock, command):
+    # check if the file exists
+    acknowledgement = struct.unpack('i', sock.recv(BUFFER))[0]
+    
+    # file exists
+    if acknowledgement == 1:
+        # ask user delete the file or not
+        confirm = input('Are you sure? ')
+        confirm = confirm.lower()
+
+        # send confirmation to server
+        sock.send(confirm.encode('utf-8'))
+
+        if confirm == 'yes':
+
+            # acknowlegement from server
+            delete = struct.unpack('i', sock.recv(BUFFER))[0]
+
+            # delete successfully
+            if delete == 1:
+                print('File deleted!')
+
+            # delete unsuccessfully
+            else:
+                print('Unable to delete the file!')
+        else:
+            print('Delete abandoned by user.')
+    
+    # file does not exist
+    else:
+        print('File does not exist!')
+
 
 
 # Main function for Part 2
@@ -110,6 +255,14 @@ def part2 ():
         elif command == 'QUIT':
             sock.close()
             break
+        elif command[:2] == 'DN':
+            dn(sock, command)
+        elif command[:2] == 'UP':
+            up(sock, command)
+        elif command[:2] == 'RM':
+            rm(sock, command)
+        else:
+            print('Failed to resolve command.')
 
 
 ############## End of Part 2 ##############
--- server/tcpserver.py
+++ server/tcpserver.py
@@ -11,7 +11,7 @@
 
 
 # Import any necessary libraries below
-import os, socket, sys, struct, subprocess
+import os, socket, sys, struct, subprocess, time
 
 
 ############## Beginning of Part 1 ##############
@@ -24,6 +24,7 @@ def init_sock(host, port):
 
     # Create a datagram socket for TCP
     try:
+        # sycn success twice
         sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     except socket.error:
         print('Failed to create socket.')
@@ -58,7 +59,7 @@ def part1 ():
     print("********** PART 1 **********")
     # Fill in the IP address of the host and the port number
     host = '192.17.61.22'
-    port = 41022
+    port = 41029
     sock, conn, addr = init_sock(host, port)
 
     # Receive message from the client
@@ -90,6 +91,107 @@ def ls(conn):
     conn.send(result.stdout)
 
 
+def dn(conn, command):
+
+    # get file name from command
+    filename = command[3:]
+
+    # check if file exists
+    result = subprocess.run(['ls', '-l', filename], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        
+    # file exists
+    if result.stdout:
+
+        # get file size and send it to client
+        file_size = int(result.stdout.decode('utf-8').split(' ')[4])
+        conn.send(struct.pack('i', file_size))
+
+        # send file
+        with open(filename, 'rb') as f:
+            for line in f:
+                conn.send(line)
+    
+    # file does not exist
+    if result.stderr:
+        conn.send(struct.pack('i', -1)) 
+
+
+def up(conn):
+
+    # ackowledgement from client
+    acknowledgement = struct.unpack('i', conn.recv(BUFFER))[0]
+
+    # file exists on client side
+    if acknowledgement == 1:
+
+        # receive file name from client
+        filename = conn.recv(BUFFER).decode('utf-8')
+
+        # check if the file already exists
+        result = subprocess.run(['ls', '-l', filename], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        
+        # file exists
+        if result.stdout:
+            # send acknowledgement to client
+            conn.send(struct.pack('i', 1))
+        # file does not exists
+        if result.stderr:
+            conn.send(struct.pack('i', -1))
+
+        # receive file size from client
+        file_size = struct.unpack('i', conn.recv(BUFFER))[0]
+
+        if file_size != -1:
+
+            f = open(filename, 'wb')
+            recv_size = 0
+
+            # start time
+            start = time.time()
+
+            # receiving file
+            while recv_size < file_size:
+                data = conn.recv(BUFFER)
+                f.write(data)
+                recv_size += len(data)
+
+            # end time
+            end = time.time()
+
+            # speed
+            speed = round(recv_size / 1000000 / (end - start), 4)
+
+            # compute the throughput and send it to client
+            throughput = '{} bytes have been transfered in {} seconds: {} Megabytes/second'.format(recv_size, round(end-start, 6), speed)
+            conn.send(throughput.encode('utf-8'))
+
+
+def rm(conn, command):
+    filename = command[3:]
+    # check if the file exists and send ackowledgement to client
+    result = subprocess.run(['ls', '-l', filename], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    
+    # file exists
+    if result.stdout:
+        conn.send(struct.pack('i', 1))
+
+        # get the confirmation from client
+        confirm = conn.recv(BUFFER).decode('utf-8')
+        if confirm == 'yes':
+            delete = subprocess.run(['rm', filename], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+
+            # delete unsuccessfully
+            if delete.stderr:
+                conn.send(struct.pack('i', -1))
+            # delete successfully
+            else:
+                conn.send(struct.pack('i', 1))
+
+    # file does not exist
+    if result.stderr:
+        conn.send(struct.pack('i', -1))
+
+
 # Main function for Part 2
 def part2 ():
     print("********** PART 2 **********")
@@ -115,6 +217,12 @@ def part2 ():
                 elif command == 'QUIT':
                     sock.close()
                     break
+                elif command[:2] == 'DN':
+                    dn(conn, command)
+                elif command[:2] == 'UP':
+                    up(conn)
+                elif command[:2] == 'RM':
+                    rm(conn, command)
                 else:
                     print('Failed to resolve command.')
 
